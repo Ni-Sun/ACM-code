218c3bb58f3b5fc7b4a19dfcbff10a7d181136e1
Ni-Sun <127489363+Ni-Sun@users.noreply.github.com>
2024-05-21 18:38:36 +0800

    Add files via upload

---
+++ To_be_tested.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+    int n, sum;
+    cin>>n;
+    sum=(n*n+n)/2;
+    cout<<sum;
+}
